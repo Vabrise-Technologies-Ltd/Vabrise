851898293fd045521d913b2a372aa4d62e5800da
root <andrewomosh44@gmail.com>
2024-01-05 12:34:29 +0300

    upgrade yarn packages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext',
+  },
+  server: {
+    mimeTypes: {
+      '.mjs': 'application/javascript',
+    },
+  },
 })
