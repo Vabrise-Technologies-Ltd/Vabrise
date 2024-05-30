ada15ac0cb88311861b9b697c09a5c71f6040379
root <andrewomosh44@gmail.com>
2024-05-30 14:20:49 +0300

    feat: add blogheader

--- src/components/Blog/BlogList.jsx
+++ src/components/Blog/BlogList.jsx
@@ -4,32 +4,37 @@ import { Link } from "react-router-dom";
 import { FetchContext } from "../Contexts/FetchContext";
 import DefaultSpinner from "../Utils/Spinner";
 
-const BlogList = () => {
-    const {blogs, errorBlogs, loadingBlogs} = useContext(FetchContext)
-    console.log(blogs)
+function getFirstWords(text, wordLimit) {
+    const words = text.split(' ');
+    if (words.length <= wordLimit) return text;
+    return words.slice(0, wordLimit).join(' ') + '...';  // Adding ellipsis to indicate truncation
+}
 
-    return ( 
+const BlogList = () => {
+    const { blogs, errorBlogs, loadingBlogs } = useContext(FetchContext);
+    console.log(blogs);
+
+    return (
         <>
             <div>
                 <p className="text-red-500">{errorBlogs}</p>
                 {!blogs || loadingBlogs ? (
-                    <div className="flex flex-col justify-center items-center md:my-32 my-12 text-cyan-500 gap-4 ">
+                    <div className="flex flex-col justify-center items-center md:my-32 my-12 text-cyan-500 gap-4">
                         <DefaultSpinner />
                         <p>Loading blogs...</p>
                     </div>
                 ) : (
                     <div className="card-container">
                         {blogs && blogs.map((blog) => (
-                            <Fade bottom>
-                                <Link to={`/articles/${blog.slug}`} key={blog.id} className="card">
-                                    <div className="md:h-56 w-full">
-                                        <img src={blog.image} alt="" />
+                            <Fade bottom key={blog.id}>
+                                <Link to={`/articles/${blog.slug}`} className="card">
+                                    <div className="md:h-52 w-full">
+                                        <img className="h-full w-full" src={blog.image} alt={blog.title} />
                                     </div>
                                     <div className="card-content">
                                         <h4 className="card-title">{blog.title}</h4>
-                                        {/* <p className="card-description">{blog.body.slice(0, 100)}...</p> */}
                                         <div
-                                            dangerouslySetInnerHTML={{ __html: blog.body.slice(0,100) }}
+                                            dangerouslySetInnerHTML={{ __html: getFirstWords(blog.body, 12) }}
                                             className="card-description"
                                         ></div>
                                     </div>
@@ -39,9 +44,8 @@ const BlogList = () => {
                     </div>
                 )}
             </div>
-            
         </>
-     );
+    );
 }
- 
+
 export default BlogList;
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,8 +1,10 @@
+import BlogHeader from "../components/Blog/BlogHeader";
 import BlogList from "../components/Blog/BlogList";
 
 const Blog = ({blogs,loading,error}) => {
     return ( 
-        <>
+        <>  
+            <BlogHeader />
             <BlogList blogs={blogs} loading={loading} error={error} />
         </>
      );
