96b3062caff412d5dedaa5ce59a42f413523617e
Andrew-Ochieng <andrewomosh44@gmail.com>
2024-06-16 13:22:32 +0300

    fix: add compressed image-assets

--- src/pages/about/sections/faq/FAQ.jsx
+++ src/pages/about/sections/faq/FAQ.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import FAQItem from './FAQItem';
+import faqImg from "../../../../assets/solutions/faq.svg"
 import { faqs } from '../../data';
 
 function FAQ() {
 
   return (
-    <div className="faq-container p-4">
-      <h1 className="text-center text-3xl font-bold md:mb-12 mb-4">Frequently Asked Questions</h1>
-      {faqs.map((faq, index) => (
-        <FAQItem key={index} question={faq.question} answer={faq.answer} />
-      ))}
+    <div className="faq-container ">
+      <h1 className="section-title md:mb-16">Frequently Asked Questions</h1>
+      <div className='grid md:grid-cols-2 md:gap-16 gap-8'>
+        <div>
+          <img src={faqImg}alt="" />
+        </div>
+        <div>
+        {faqs.map((faq, index) => (
+          <FAQItem key={index} question={faq.question} answer={faq.answer} />
+        ))}
+        </div>
+      </div>
     </div>
   );
 }
